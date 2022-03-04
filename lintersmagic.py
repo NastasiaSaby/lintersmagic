74fd16f316b6ee7acf45b14c28fd16752d2e3cd2
Nastasia <nastasia.saby@bedrockstreaming.com>
2022-03-04 12:36:56 +0100

    Ref: readable

--- lintersmagic.py
+++ lintersmagic.py
@@ -12,7 +12,6 @@ import tempfile
 import io
 import os
 import logging
-import copy
 import pycodestyle as pycodestyle_module
 
 from IPython.core.magic import register_cell_magic
@@ -22,15 +21,12 @@ from IPython.core import magic_arguments
 vw = None
 init_pycodestyle = False
 ignore_codes = []
-ignore_codes_bak = copy.deepcopy(ignore_codes)
 max_line_length = 79
-max_line_length_bak = copy.deepcopy(max_line_length)
 
 
 class VarWatcher(object):
     def __init__(self, ip):
         self.shell = ip
-        self.last_x = None
 
     def auto_run_pycodestyle(self, result):
         pycodestyle(1, result.info.raw_cell, auto=True)
@@ -38,22 +34,22 @@ class VarWatcher(object):
             print('Error before execution: %s' % result.error_before_exec)
 
 
-def load_ipython_extension(ip, pck=False):
+def load_ipython_extension(ipython, pck=False):
     # The `ipython` argument is the currently active `InteractiveShell`
     # instance, which can be used in any way. This allows you to register
     # new magics or aliases, for example.
     if pck == False:
         global vw
-        vw = VarWatcher(ip)
+        vw = VarWatcher(ipython)
     if pck == 'pycodestyle':
-        ip.events.register('post_run_cell', vw.auto_run_pycodestyle)
+        ipython.events.register('post_run_cell', vw.auto_run_pycodestyle)
     pass
 
 
-def unload_ipython_extension(ip, pck=False):
+def unload_ipython_extension(ipython, pck=False):
     # If you want your extension to be unloadable, put that logic here.
     if pck == 'pycodestyle':
-        ip.events.unregister('post_run_cell', vw.auto_run_pycodestyle)
+        ipython.events.unregister('post_run_cell', vw.auto_run_pycodestyle)
         global init_pycodestyle
         init_pycodestyle = False
     pass
@@ -69,18 +65,13 @@ def pycodestyle_on(line):
     args = magic_arguments.parse_argstring(pycodestyle_on, line)
     # check ignore codes
     global ignore_codes
-    global ignore_codes_bak
-    ignore_codes = ignore_codes_bak
     if args.ignore:
         ignore_codes = list(set(ignore_codes + args.ignore.split(',')))
 
     # check max-line-length
     global max_line_length
-    global max_line_length_bak
-    max_line_length = max_line_length_bak
     if args.max_line_length:
         max_line_length = int(args.max_line_length)
-
     load_ipython_extension(vw.shell, pck='pycodestyle')
 
 @register_line_magic
@@ -96,11 +87,8 @@ def pycodestyle(line, cell, auto=False):
         init_pycodestyle = True
 
     # output is written to stdout
-    # remember and replace
-    old_stdout = sys.stdout
-    # temporary replace
     sys.stdout = io.StringIO()
-    # store code in a file, todo unicode
+    # store code in a file
     if cell.startswith(('!', '%%', '%')):
         return
     with tempfile.NamedTemporaryFile(mode='r+', delete=False) as file:
@@ -128,7 +116,6 @@ def pycodestyle(line, cell, auto=False):
             add = 0
         logging.warning('{}:{}:{}'.format(int(line) + add, col, error))
         # restore
-    sys.stdout = old_stdout
     try:
         os.remove(file.name)
     except OSError as e:  ## if failed, report it back to the user ##
